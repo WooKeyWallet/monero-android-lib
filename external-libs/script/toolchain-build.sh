ddba14103dc02e44940939aed0b8bde31953b48c
WooKey Wallet <46467247+WooKeyWallet@users.noreply.github.com>
2019-04-28 11:04:39 +0800

    Update toolchain-build.sh
    remove x86 and x86_64

--- external-libs/script/toolchain-build.sh
+++ external-libs/script/toolchain-build.sh
@@ -8,7 +8,7 @@ build_root=$EXTERNAL_LIBS_BUILD_ROOT
 PATH=$ANDROID_NDK_ROOT/build/tools/:$PATH
 
 args="--api 21 --stl=libc++"
-archs=(arm arm64 x86 x86_64)
+archs=(arm arm64)
 
 for arch in ${archs[@]}; do
     if [ ! -d "$NDK_TOOL_DIR/$arch" ]; then
